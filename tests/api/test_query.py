90f34f2ece5f0ef1a754e2dce22d1acb7d36fea3
Patrick Arminio <patrick.arminio@gmail.com>
2022-09-06 15:46:01 +0100

    Working test

--- tests/api/test_query.py
+++ tests/api/test_query.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI
 from fastapi.testclient import TestClient
+
 from main import app
+
 
 client = TestClient(app)
 
 
 def test_read_main():
-    response = client.post("/graphql", json={"query": "{ hello }"})
+    response = client.post("/graphql", json={"query": "{ foo(id: 1) { id name } }"})
     assert response.status_code == 200
-    assert response.json() == {"data": {"hello": "Hello World"}}
+    assert response.json() == {"data": {"foo": {"id": "1", "name": "Foo"}}}
