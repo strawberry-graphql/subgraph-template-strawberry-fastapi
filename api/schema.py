4bf660e1dcf70f671b73ec78d98b128606d030a0
Patrick Arminio <patrick.arminio@gmail.com>
2022-09-06 11:21:35 +0100

    Update

--- api/schema.py
+++ api/schema.py
@@ -1,5 +1,4 @@
 import strawberry
-from strawberry.file_uploads import Upload
 
 
 @strawberry.type
@@ -8,11 +7,12 @@ class Query:
     def hello(self) -> str:
         return "Hello World"
 
+
 @strawberry.type
 class Mutation:
     @strawberry.mutation
-    def create_user(self, name: str, file: Upload) -> str:
-        return str(file)
+    def create_user(self, name: str) -> str:
+        return name
 
 
 schema = strawberry.Schema(Query, Mutation)
