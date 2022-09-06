bc4724969e7c4cdcce89d838e4aad87b983fd80b
Patrick Arminio <patrick.arminio@gmail.com>
2022-09-06 11:36:03 +0100

    Initial implementation of an example schema

--- api/schema.py
+++ api/schema.py
@@ -1,11 +1,35 @@
+from typing import Optional
 import strawberry
+
+from strawberry.schema_directive import Location
+
+
+@strawberry.schema_directive(locations=[Location.SCHEMA])
+class Contact:
+    name: str = strawberry.field(description="Contact title of the subgraph owner")
+    url: Optional[str] = strawberry.field(
+        description="URL where the subgraph's owner can be reached"
+    )
+    description: Optional[str] = strawberry.field(
+        description="Other relevant notes can be included here; supports markdown links"
+    )
+
+
+@strawberry.federation.type(keys=["id"])
+class Foo:
+    id: strawberry.ID
+    name: Optional[str]
+
+    @classmethod
+    def resolve_reference(cls, representation: dict) -> "Foo":
+        return cls(id=strawberry.ID("1"), name="Foo")
 
 
 @strawberry.type
 class Query:
     @strawberry.field
-    def hello(self) -> str:
-        return "Hello World"
+    def foo(self, id: strawberry.ID) -> Optional[Foo]:
+        return Foo(id=id, name="Foo")
 
 
 @strawberry.type
@@ -15,4 +39,14 @@ class Mutation:
         return name
 
 
-schema = strawberry.Schema(Query, Mutation)
+schema = strawberry.federation.Schema(
+    Query,
+    Mutation,
+    schema_directives=[
+        Contact(
+            name="FooBar Server Team",
+            url="https://myteam.slack.com/archives/teams-chat-room-url",
+            description="send urgent issues to [#oncall](https://yourteam.slack.com/archives/oncall).",
+        )
+    ],
+)
