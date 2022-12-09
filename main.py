9bb748108724f292102d82fecccd3759bc3a0535
Patrick Arminio <patrick.arminio@gmail.com>
2022-12-09 14:16:59 +0000

    Add CORS support for Apollo Studio

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from strawberry.fastapi import GraphQLRouter
 
 from api.schema import schema
 
-graphql_app = GraphQLRouter(schema, path="/")
 
 app = FastAPI()
+
+origins = [
+    "https://studio.apollographql.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+graphql_app = GraphQLRouter(schema, path="/")
+
 app.include_router(graphql_app)
